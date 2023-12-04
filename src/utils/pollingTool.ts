d8b049da28795492dc6caa5b37ba31b6d61637da
PavloDolia <123400767+PavloDolia@users.noreply.github.com>
2023-12-04 22:22:40 +0200

    feature/SCRUM 41 -  polling tool (#3)
    * polling tool v1
    
    * Update pollingTool.ts
    
    interval in seconds
    
    * polling tool with setInterval
    
    * polling tool

---
+++ src/utils/pollingTool.ts
@@ -0,0 +1,28 @@
+class PollingTool {
+  private intervalObject?: ReturnType<typeof setInterval>;
+
+  constructor(
+    private callback: () => any,
+    private interval: number,
+    private stopTrigger?: () => boolean,
+  ) {
+    this.callback = callback;
+    this.interval = interval;
+    if (stopTrigger) this.stopTrigger = stopTrigger;
+  }
+
+  start() {
+    this.intervalObject = setInterval(() => {
+      if (this.stopTrigger && this.stopTrigger()) {
+        return this.stop();
+      }
+      this.callback();
+    }, this.interval);
+  }
+
+  stop() {
+    if (this.intervalObject) clearInterval(this.intervalObject);
+  }
+}
+
+export default PollingTool;
