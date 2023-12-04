d2951f73acc446f00fa13df3ee573aef35c3682e
PavloDolia <paveldolia93@gmail.com>
2023-12-04 11:35:20 +0200

    polling tool v1

---
+++ src/utils/pollingTool.ts
@@ -0,0 +1,36 @@
+class PollingTool {
+  private continuePolling = true;
+  private intervalObject?: ReturnType<typeof setTimeout>;
+
+  constructor(
+    private interval: number,
+    private callback: () => Promise<unknown>,
+    private stopTrigger?: () => boolean,
+  ) {
+    this.interval = interval;
+    this.callback = callback;
+    if (stopTrigger) this.stopTrigger = stopTrigger;
+  }
+
+  async start() {
+    try {
+      await this.callback();
+    } catch (error) {
+      console.log(error);
+      this.stop();
+    }
+
+    if (this.stopTrigger && this.stopTrigger()) return;
+
+    if (this.continuePolling)
+    this.intervalObject = setTimeout(this.start, this.interval);
+  }
+
+  stop() {
+    this.continuePolling = false;
+    if (this.intervalObject) clearTimeout(this.intervalObject);
+  }
+
+}
+
+export default PollingTool;
