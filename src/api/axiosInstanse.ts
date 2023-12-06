079501ababd2c8535b230348cfec9ac3efaecb4d
PavloDolia <paveldolia93@gmail.com>
2023-12-06 15:30:21 +0200

    fix axios instance

--- src/api/axiosInstanse.ts
+++ src/api/axiosInstanse.ts
@@ -36,7 +36,7 @@ axiosInstance.interceptors.response.use(
     } else {
       store.dispatch(clearTokens());
       history.push('/login');
-      return Promise.reject(error);
+      throw error;
     }
   },
 );
