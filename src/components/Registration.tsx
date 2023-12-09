ee4b63fbc6922f777993c16ec5b970463aeb8f1a
Ivan Holotsvan <iholotsvan@gmail.com>
2023-12-09 21:05:54 +0200

    Reformat code

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,11 +1,8 @@
 import React,{useState} from "react";
-import { styled } from "@linaria/react";
 import SignUp from "./regestration/SignUp";
 import LogIn from "./regestration/LogIn";
 
-type ToggleProps = {
-  handleToggle: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
-}
+
 
 const RegistrationForm: React.FC =()=>{
   const [isToggled,setIsToggled] = useState(false);
