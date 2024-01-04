ae7886794f4efd8fb6b6e14d9872801640a928f5
vnglnk <128087718+holotsvan@users.noreply.github.com>
2024-01-04 17:04:34 +0200

    Devops/scrum 83 pre commit prettier eslint (#33)
    * pre-commit test
    
    * Ivan Durak
    
    * buttom component
    
    * Crear after pre-commit hook test
    
    * Add gitignore
    
    * Delete filling of .eslintcache

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,47 +1,38 @@
-import React from "react";
-import {greyColor,primaryColor,whiteColor} from './colors'
-import { styled } from "@linaria/react";
+import React from 'react'
+import { greyColor, primaryColor, whiteColor } from './colors'
+import { styled } from '@linaria/react'
 
 //////////////////////
-// BUTTON 
+// BUTTON
 //////////////////////
-interface ButtonProps{
-    children:React.ReactNode;
-    onClick?:(event:React.MouseEvent<HTMLButtonElement>) => void;
+interface ButtonProps {
+  children: React.ReactNode
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 const StyledButton = styled.button`
-background-color: ${primaryColor};
-color: ${whiteColor};
-font-size: 14px;
-font-style: normal;
-font-weight: 700;
-line-height: 140%;
-width:100%;
-padding:11px 0;
-border: none;
-border-radius: 8px;
-cursor: pointer;
-transition: all 0.2s ease-in-out;
-&.disabled{
-  background-color:${greyColor};
-}
-:hover {
-  opacity:0.9;
-}
-// @media only screen and (max-height: 450px) and (orientation:landscape){    
-//   font-size:14px;
-//   line-height:1;
-// }
-`;
+  background-color: ${primaryColor};
+  color: ${whiteColor};
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 140%;
+  width: 100%;
+  padding: 11px 0;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  &.disabled {
+    background-color: ${greyColor};
+  }
+  :hover {
+    opacity: 0.9;
+  }
+`
 
-const Button: React.FC<ButtonProps> = ({children,onClick}) => {
-  return(
-    <StyledButton onClick={onClick}>
-        {children}
-    </StyledButton>
-  ) 
-    
-};
+const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
+  return <StyledButton onClick={onClick}>{children}</StyledButton>
+}
 
-export default Button;
+export default Button
