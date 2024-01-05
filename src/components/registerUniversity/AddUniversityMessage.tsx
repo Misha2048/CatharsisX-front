4204c050309eda532dbed8505ae985b5fb45a9e5
PavloDolia <paveldolia93@gmail.com>
2024-01-05 22:18:55 +0200

    added register university modal window

---
+++ src/components/registerUniversity/AddUniversityMessage.tsx
@@ -0,0 +1,15 @@
+import { styled } from '@linaria/react'
+
+const AddUniversityMessage = styled.p`
+  font-family: 'Inter', sans-serif;
+  color: #fff;
+  font-size: 16px;
+  line-height: 1.1;
+  padding-right: 48px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 18px;
+  }
+`
+
+export default AddUniversityMessage
