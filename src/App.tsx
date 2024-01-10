02aa13cdb3e473bacc99f833a915beab406af264
PavloDolia <paveldolia93@gmail.com>
2024-01-10 17:43:18 +0200

    added header to pages

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,21 @@ import LogIn from '@components/regestration/LogIn'
 import ForgotPasswordForm from '@components/ForgotPasswordForm'
 import ResetPasswordForm from '@components/ResetPasswordForm'
 import Stillage from '@pages/Stillage'
+import Header from '@components/Header'
 
 dayjs.locale('en-gb')
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path='stillage/:id' element={<Stillage />} />
+      </Route>
       <Route path='/login' element={<LogIn />} />
       <Route path='/signup' element={<SignUp />} />
       <Route path='/forgot-password' element={<ForgotPasswordForm />} />
       <Route path={`/password-reset/:id`} element={<ResetPasswordForm />} />
-      <Route path='/stillage/:id' element={<Stillage />} />
     </Routes>
   )
 }
