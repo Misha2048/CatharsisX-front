495be04b923f9b6eef809289da50ba2793c7e0d1
vnglnk <128087718+holotsvan@users.noreply.github.com>
2024-01-17 17:09:57 +0200

    Change z-index on ModalBody  BlackOverlay and BurgerIcon (#48)

--- src/components/BlackOverlay.tsx
+++ src/components/BlackOverlay.tsx
@@ -12,7 +12,7 @@ const BlackOverlay = styled.div<Props>`
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.5);
   backdrop-filter: blur(4px);
-  z-index: 4;
+  z-index: 400;
   transition: all 0.3s;
   visibility: ${(props) => (props.show ? 'visible' : 'hidden')};
   opacity: ${(props) => (props.show ? '1' : '0')};
