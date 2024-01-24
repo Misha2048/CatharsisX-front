81732d66ce4fc86c54f413a33ec207ae82f7c10a
PavloDolia <paveldolia93@gmail.com>
2024-01-24 21:57:28 +0200

    fix refresh tokens

--- src/api/axiosInstanse.ts
+++ src/api/axiosInstanse.ts
@@ -24,6 +24,10 @@ axiosInstance.interceptors.response.use(
   (response) => response,
 
   async (error) => {
+    if (error.response.status === 403) {
+      return error.response
+    }
+
     if (
       error.response.status !== 401 &&
       error.response.status >= 400 &&
@@ -34,15 +38,17 @@ axiosInstance.interceptors.response.use(
     }
 
     const originalRequest = { ...error.config }
-    originalRequest._isRetry = true
     const refreshToken = localStorage.getItem('refreshToken')
     if (error.response.status === 401 && !error.config._isRetry && refreshToken) {
+      originalRequest._isRetry = true
       try {
         const resp = await axiosInstance.get('/auth/refresh', {
           headers: { Authorization: `Bearer ${refreshToken}` },
           _isRetry: true,
         } as ConfigType)
-        store.dispatch(setTokens(resp.data))
+        if (resp.data.accessToken && resp.data.refreshToken) {
+          store.dispatch(setTokens(resp.data))
+        }
         originalRequest.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`
         return axiosInstance.request(originalRequest)
       } catch (error) {
