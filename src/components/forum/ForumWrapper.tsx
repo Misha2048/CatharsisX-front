f43aa068e961b48b112ac6404338855873035336
PavloDolia <paveldolia93@gmail.com>
2024-02-29 14:02:11 +0200

    fixed pagination buttons location

--- src/components/forum/ForumWrapper.tsx
+++ src/components/forum/ForumWrapper.tsx
@@ -3,6 +3,9 @@ import { styled } from '@linaria/react'
 const ForumWrapper = styled.div`
   max-width: 1312px;
   margin: 0px auto;
+  min-height: inherit;
+  display: flex;
+  flex-direction: column;
 `
 
 export default ForumWrapper
